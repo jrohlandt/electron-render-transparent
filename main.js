f43d03db3d74d817e09cd96485718db3008fd12e
Jeandré Rohlandt <4688503+jrohlandt@users.noreply.github.com>
2022-11-23 09:43:24 +0200

    Transparent secondary window overlayed on primary window

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
+const path = require("path");
+
+app.disableHardwareAcceleration();
+
+let mainWindow;
+let secondaryWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+  });
+  mainWindow.loadFile("./index.html");
+  mainWindow.webContents.setFrameRate(60);
+}
+
+function createSecondaryWindow() {
+  secondaryWindow = new BrowserWindow({
+    webPreferences: { offscreen: false },
+    frame: false,
+    transparent: true,
+  });
+  secondaryWindow.loadFile("./secondary.html");
+  secondaryWindow.webContents.setFrameRate(60);
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+  createSecondaryWindow();
+
+  secondaryWindow.webContents.on("paint", (event, dirty, image) => {
+    fs.writeFileSync("ex.png", image.toPNG());
+  });
+
+  console.log(
+    `The screenshot has been successfully saved to ${path.join(
+      process.cwd(),
+      "ex.png"
+    )}`
+  );
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
